50e3286d9446f79f050e001c1d1ffab356fc6436
DeniAlimkhanov <Alimkhanov.d.r@gmail.com>
2026-01-26 21:18:55 +0300

    Feat: День 1 - Классы, объекты, конструкторы (Person, Car)

---
+++ src/ru/week2/day1/Car.java
@@ -0,0 +1,36 @@
+package ru.week2.day1;
+
+public class Car {
+    private String brand;
+    private String color;
+    private int year;
+    private int speed;
+
+    public Car (String brand, String color, int year, int speed){
+        this.brand = brand;
+        this.color = color;
+        this.year = year;
+        this.speed = speed;
+        speed = 0;
+    }
+    public void accelerate() {
+        speed += 10;
+        System.out.println("Speed: " + speed);
+    }
+    public void brake() {
+        speed -= 10;
+        System.out.println("Speed: " + speed);
+    }
+    public void getInfo() {
+        System.out.println("Марка машины: " + brand + ", Цвет: " + color + ", Год выпуска: " + year + ", Скорость: " + speed);
+    }
+}
+class Rain {
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "red", 2015, 200);
+        car.getInfo();
+        car.brake();
+        car.getInfo();
+
+    }
+}
---
+++ src/ru/week2/day1/Person.java
@@ -0,0 +1,31 @@
+package ru.week2.day1;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Person {
+    private String name;
+    private int age;
+    private String email;
+
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+    public void getInfo() {
+        System.out.println("Имя: " + name + ", Возраст: " + age + ", Почта: " + email);
+    }
+    public void haveBirthday() {
+        age++;
+        System.out.println("Исполняется " + age + " лет");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Дени", 18, "alidenchik@mail.ru");
+        person.getInfo();
+        person.haveBirthday();
+        person.getInfo();
+    }
+}
